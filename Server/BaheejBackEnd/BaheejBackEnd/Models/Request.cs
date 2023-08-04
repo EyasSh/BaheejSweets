575cfcb3cabd4d969067dbd9537633a7351684c0
unknown <eyas.wt@gmail.com>
2023-08-04 15:26:46 +0300

    initial

---
+++ Server/BaheejBackEnd/BaheejBackEnd/Models/Request.cs
@@ -0,0 +1,10 @@
+﻿namespace BaheejBackEnd.Models
+{
+    public class Request
+    {
+        public string _ProductId { get; set; }
+        public string _ProductName {  get; set; }
+
+        
+    }
+}
---
+++ Server/BaheejBackEnd/BaheejBackEnd/Models/User.cs
@@ -0,0 +1,17 @@
+﻿namespace BaheejBackEnd.Models
+{
+    public class User
+    {
+        public string _Id { get; init; }
+        public string _Name { get; set; }
+
+        public string _PhoneNumber { get; set; }
+
+        public User(string id, string name, string phoneNumber)
+        {
+            _Id = id;
+            _Name = name;
+            _PhoneNumber = phoneNumber;
+        }
+    }
+}
