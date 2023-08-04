6bbbff4aa63415796938b059af85c6921a5e7c51
unknown <eyas.wt@gmail.com>
2023-08-04 19:39:43 +0300

    MDBWrapperPreppped

---
+++ Server/BaheejBackEnd/BaheejBackEnd/Controllers/RequestController.cs
@@ -0,0 +1,16 @@
+﻿using BaheejBackEnd.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace BaheejBackEnd.Controllers
+{
+    [ApiController]
+    [Route("[Requests]")]
+    public class RequestController: ControllerBase
+    {
+
+        public RequestController() { }  
+
+        
+    }
+}
--- Server/BaheejBackEnd/BaheejBackEnd/Models/Request.cs
+++ Server/BaheejBackEnd/BaheejBackEnd/Models/Request.cs
@@ -6,16 +6,21 @@ namespace BaheejBackEnd.Models
     {
         //a class simulating a cake/ sweet requests
         public string _ProductId { get; init; }
+
+        public string _ClientId { get; init; }
+        public string _ClientName { get; init; }
         public string _ProductName {  get; set; }
         public string _ProductImageRoute { get; init; }
         //TO DO:
         // put an Object for the product image itself here
 
         //Constructor
-        public Request(string id, string name, string imageRoute) 
+        public Request(string id, string clientId, string clientName,string productName, string imageRoute) 
         {
+             _ClientId = clientId;
+            _ClientName = clientName;
             _ProductId = id;
-            _ProductName = name;
+            _ProductName = productName;
             _ProductImageRoute = imageRoute;
         }
 
