4d4cb39b87cc899c1b64958bd97b9ce0931bc667
unknown <eyas.wt@gmail.com>
2023-08-04 15:53:22 +0300

    requests constructor

--- Server/BaheejBackEnd/BaheejBackEnd/Models/Request.cs
+++ Server/BaheejBackEnd/BaheejBackEnd/Models/Request.cs
@@ -1,9 +1,23 @@
-﻿namespace BaheejBackEnd.Models
+﻿
+
+namespace BaheejBackEnd.Models
 {
     public class Request
     {
-        public string _ProductId { get; set; }
+        //a class simulating a cake/ sweet requests
+        public string _ProductId { get; init; }
         public string _ProductName {  get; set; }
+        public string _ProductImageRoute { get; init; }
+        //TO DO:
+        // put an Object for the product image itself here
+
+        //Constructor
+        public Request(string id, string name, string imageRoute) 
+        {
+            _ProductId = id;
+            _ProductName = name;
+            _ProductImageRoute = imageRoute;
+        }
 
         
     }
